1120d1eff423058fded35d7e8fec8609e5368a34
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-26 17:03:50 +0100

    binding socket

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 import socket
 import sys
 
+HOST = ''
+PORT = 50000
+
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error, msg:
+    s.bind((HOST, PORT))
+except (socket.error , msg):
     print('Failed to create socket. Error code: ' + str(msg[0]))
     print('Error message: ' + msg[1])
     sys.exit()
-    
-print('Socket Created')
+print('Socked created and binding complete')
