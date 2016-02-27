a7c724980fb97f42240049828186e147bfbe8c60
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-27 08:17:09 +0100

    checking frame for M pakiets

--- server.py
+++ server.py
@@ -20,13 +20,18 @@ print('Socked created and binding complete')
 s.listen(BACKLOG)
 print('Server listening')
 
+
+clients = {"other": {"x": 0, "y": 0}}
+
 def clientThread(conn):
     try:
         while 1:
             data = conn.recv(SIZE)
-            reply = ('Ok...' + data)
+            if data.startswith("M"):
+                _, uuid, x, y = data.split(':')
+                clients[uuid] = {"x": x, "y": y}
             if data:
-                conn.send(reply)
+                conn.send("")
         conn.close()
     except Exception as e:
         print e.message
