65c32364a1c33abcbcc0f720f98bb909b6c3da73
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-03-05 12:15:29 +0100

    new version of server, waiting for players, sending other players postion

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-import socket
 import sys
+import time
+import socket
 from thread import *
 
 
@@ -23,26 +24,53 @@ print('Server listening')
 
 clients = {}
 
+
 def clientThread(conn):
-    my_uuid = ""
     try:
         while 1:
-            twoPlayerConnected = False
-            data = conn.recv(SIZE)
-            client = {}
-            if data.startswith("M"):
-                _, uuid, x, y, r = data.split(':')
-                my_uuid = uuid
-                clients[uuid] = "x:%s:y:%s:r:%s" % (x,y,r)
-            if len(clients) == 2:
-                twoPlayerConnected = True
-                for key in clients:
-                    if key is not my_uuid:
-                        data = clients[key]
-            if data and twoPlayerConnected:
-                conn.send(data)
-            else:
-                conn.send("y:142.0:x:160.0:r:0.0")
+            packet = conn.recv(SIZE)
+
+            _data = packet.split(':')
+            kind = _data[0]
+            uuid = _data[1]
+            data = _data[2:-1]
+            client_time = float(_data[-1])
+            server_time = time.time()
+
+            print "-->", packet, "ping", server_time - client_time
+
+            back = ""
+
+            if kind == "W":
+                others = [c for c in clients.keys() if c != uuid]
+                position = data
+                if not others:
+                    pair = None
+                else:
+                    pair = others[0]
+
+                clients[uuid] = {"state": "W", "pair": pair, "position": position}
+
+                back = "W:%d:%s:%f" % (
+                    1 if pair else 0,
+                    ":".join(position),
+                    server_time)
+
+            if kind == "M":
+                client = clients[uuid]
+                client['position'] = data
+
+                pair = client['pair']
+                pair_position = clients[pair]['position']
+
+                back = "M:%s:%f" % (":".join(pair_position), server_time)
+
+            if not back:
+                assert "bad packet"
+
+            print "<---", back, uuid
+            conn.send(back)
+
         conn.close()
 
     except Exception as e:
@@ -51,6 +79,7 @@ def clientThread(conn):
 while 1:
     (conn, addr) = s.accept()
     print('Connected with ' + addr[0] + ':' + str(addr[1]))
-    start_new_thread(clientThread ,(conn,))
+    start_new_thread(clientThread, (conn, ))
+
 
 s.close()
